2daaabaf22d3d578362a3f26d6e2db5cee64cad3
thmiya-pub <hayato1203game@gmail.com>
2024-11-09 15:46:17 +0900

    index修正

--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -29,6 +29,6 @@ CREATE TABLE `visit_history` (
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`),
-  INDEX `idx_created_at` (`created_at`);
-  CREATE INDEX `idx_visit_history_tenant_competition_player` (`tenant_id`, `competition_id`, `player_id`, `created_at`)
+  INDEX `idx_created_at` (`created_at`),
+  INDEX `idx_visit_history_tenant_competition_player` (`tenant_id`, `competition_id`, `player_id`, `created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
